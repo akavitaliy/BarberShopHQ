0befd3481e434244f07b28ebfb9b712644ac4884
VitamiN <scoundrel98@gmail.com>
2021-04-01 20:12:12 +0300

    add langth validation

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require 'rake'
 set :database, {adapter: "sqlite3", database: "barber.db"}
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true
+	validates :name, presence: true, length: { minimum: 3 }
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true
